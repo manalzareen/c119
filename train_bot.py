628d29b9a2e4c1aef39deb36362ee4e5f54e1f2d
manalzareen <81857933+manalzareen@users.noreply.github.com>
2022-12-08 20:07:00 +0530

    Add files via upload

--- train_bot.py
+++ train_bot.py
@@ -56,6 +56,38 @@ stem_words, classes = create_bot_corpus(stem_words,classes)
 print(stem_words)
 print(classes)
 
+training_data = []
+number_of_tags = len(classes)
+labels = [0]*number_of_tags
+
+
 #Create Bag Of Words
+for word_tags in word_tags_list:
+    bag_of_words = []       
+    pattern_words = word_tags[0]
+    for word in pattern_words:
+        index=pattern_words.index(word)
+        word=stemmer.stem(word.lower())
+        pattern_words[index]=word
+    for word in stem_words:
+        if word in pattern_words:
+            bag_of_words.append(1)
+        else :
+            bag_of_words.append(0)
+    labels_encoding = list(labels) #labels all zeroes initially
+    tag = word_tags[1] #save tag
+    tag_index = classes.index(tag)  #go to index of tag
+    labels_encoding[tag_index] = 1  #append 1 at that index
+    
+    training_data.append([bag_of_words, labels_encoding])
 
 #Create training data
+def preprocess_train_data(training_data):
+    training_data=np.array(training_data)
+    train_x = list(training_data[:,0])
+    train_y = list(training_data [:,1])
+    print(train_x[0])
+    print(train_y[0])
+    return train_x, train_y
+
+train_x,train_y = preprocess_train_data(training_data)
